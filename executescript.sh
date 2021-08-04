744c5b4a8f6d36017d8afc5054d081769dcbf7d0
Eyal Perlmuter <56922923+eyalperlm@users.noreply.github.com>
2021-08-04 13:15:25 +0300

    Update executescript.sh

--- executescript.sh
+++ executescript.sh
@@ -1,3 +1,3 @@
-curl https://raw.githubusercontent.com/cloudshell-colony/setup/master/azure.sh  > colony.sh
+curl https://raw.githubusercontent.com/qualitorque/setup/master/azure.sh  > colony.sh
 chmod +x colony.sh
 ./colony.sh 
